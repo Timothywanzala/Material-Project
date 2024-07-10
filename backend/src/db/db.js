8305f624166d36263679144c31ed61d4209eee80
Emorut Deogratius <emorutdeogratius@gmail.com>
2024-07-10 17:45:35 +0300

    Update Database config
    Update Database config

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,11 +1,16 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.FLER_DB_NAME, process.env.FLER_DB_USERNAME, '', {
-  host: process.env.FLER_DB_HOST,
-  dialect:'mysql',
-  define: {
-    timestamps: false
+const sequelize = new Sequelize(
+  process.env.FLER_DB_NAME,
+  process.env.FLER_DB_USERNAME,
+  process.env.FLER_DB_PASSWORD,
+  {
+    host: process.env.FLER_DB_HOST,
+    dialect: "mysql",
+    define: {
+      timestamps: false,
+    },
   }
-});
+);
 
-module.exports = sequelize ;
+module.exports = sequelize;

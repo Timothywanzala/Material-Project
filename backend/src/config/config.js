41ba885e4d19e54d67a8414fbed24a37832ac50c
Emorut Deogratius <emorutdeogratius@gmail.com>
2024-07-10 17:55:53 +0300

    Update Config.js
    Update Config.js

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,33 +1,32 @@
 module.exports = {
-    development: {
-      username: process.env.FLER_DB_USERNAME,
-      password: '',
-      database: process.env.FLER_DB_NAME,
-      host: process.env.FLER_DB_HOST,
-      dialect: 'mysql',
-      define: {
-        timestamps: false
-      }
+  development: {
+    username: process.env.FLER_DB_USERNAME,
+    password: process.env.FLER_DB_PASSWORD,
+    database: process.env.FLER_DB_NAME,
+    host: process.env.FLER_DB_HOST,
+    dialect: "mysql",
+    define: {
+      timestamps: false,
     },
-    test: {
-      username: 'root',
-      password: null,
-      database: 'database_test',
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      define: {
-        timestamps: false
-      }
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    define: {
+      timestamps: false,
     },
-    production: {
-      username: 'root',
-      password: null,
-      database: 'database_production',
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      define: {
-        timestamps: false
-      }
-    }
-  };
-  
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    define: {
+      timestamps: false,
+    },
+  },
+};
